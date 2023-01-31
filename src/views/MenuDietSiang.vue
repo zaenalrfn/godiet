b984887883c2e62cb213f59229c199229febe280
zaenalrfn <ZAOfficial24@gmail.com>
2023-01-31 21:47:43 +0700

    menu diet siang

--- src/views/MenuDietSiang.vue
+++ src/views/MenuDietSiang.vue
@@ -20,27 +20,120 @@
       <div class="row justify-content-center gap-4 mb-3 pb-3">
         <div
           class="cardMenu"
-          v-for="makanan in menuDietSiang"
+          v-for="makanan in menuDietSiang_1"
           v-bind:key="makanan.idMeal"
         >
           <div class="d-flex justify-content-center">
             <img :src="makanan.strMealThumb" />
           </div>
           <div class="lh-sm" id="text">
-            <h4>{{ makanan.strMeal }}</h4>
+            <h4 class="mt-2">{{ makanan.strMeal }}</h4>
             <p>Informasi Nutrisi</p>
           </div>
           <div class="row justify-content-center text-center" id="info">
             <div class="col-3">
-              <h3>{{}}</h3>
+              <h3>{{ nutrisiMenu[0].menu_1.calories }}</h3>
               <p>Kalori</p>
             </div>
             <div class="col-3">
-              <h3>7,6</h3>
+              <h3>{{ nutrisiMenu[0].menu_1.carbo }}</h3>
               <p>Karbo</p>
             </div>
             <div class="col-3">
-              <h3>{{}}</h3>
+              <h3>{{ nutrisiMenu[0].menu_1.protein }}</h3>
+              <p>Protein</p>
+            </div>
+            <div class="col-3">
+              <h3>1</h3>
+              <p>Porsi</p>
+            </div>
+          </div>
+        </div>
+        <div
+          class="cardMenu"
+          v-for="makanan_2 in menuDietSiang_2"
+          v-bind:key="makanan_2.food.foodId"
+        >
+          <div class="d-flex justify-content-center">
+            <img :src="makanan_2.food.image" />
+          </div>
+          <div class="lh-sm" id="text">
+            <h4 class="mt-2">{{ makanan_2.food.label }}</h4>
+            <p>Informasi Nutrisi</p>
+          </div>
+          <div class="row justify-content-center text-center" id="info">
+            <div class="col-3">
+              <h3>{{ nutrisiMenu[0].menu_2.calories }}</h3>
+              <p>Kalori</p>
+            </div>
+            <div class="col-3">
+              <h3>{{ nutrisiMenu[0].menu_2.carbo }}</h3>
+              <p>Karbo</p>
+            </div>
+            <div class="col-3">
+              <h3>{{ nutrisiMenu[0].menu_1.protein }}</h3>
+              <p>Protein</p>
+            </div>
+            <div class="col-3">
+              <h3>1</h3>
+              <p>Porsi</p>
+            </div>
+          </div>
+        </div>
+        <div
+          class="cardMenu"
+          v-for="makanan_3 in menuDietSiang_3"
+          v-bind:key="makanan_3.idMeal"
+        >
+          <div class="d-flex justify-content-center">
+            <img :src="makanan_3.strMealThumb" />
+          </div>
+          <div class="lh-sm" id="text">
+            <h4 class="mt-2">{{ makanan_3.strMeal }}</h4>
+            <p>Informasi Nutrisi</p>
+          </div>
+          <div class="row justify-content-center text-center" id="info">
+            <div class="col-3">
+              <h3>{{ nutrisiMenu[0].menu_3.calories }}</h3>
+              <p>Kalori</p>
+            </div>
+            <div class="col-3">
+              <h3>{{ nutrisiMenu[0].menu_3.carbo }}</h3>
+              <p>Karbo</p>
+            </div>
+            <div class="col-3">
+              <h3>{{ nutrisiMenu[0].menu_3.protein }}</h3>
+              <p>Protein</p>
+            </div>
+            <div class="col-3">
+              <h3>1</h3>
+              <p>Porsi</p>
+            </div>
+          </div>
+        </div>
+        <div
+          class="cardMenu"
+          v-for="makanan_4 in menuDietSiang_4"
+          v-bind:key="makanan_4.food.foodId"
+        >
+          <div class="d-flex justify-content-center">
+            <img :src="makanan_4.food.image" />
+          </div>
+          <div class="lh-sm" id="text">
+            <h4 class="mt-2">{{ makanan_4.food.label }}</h4>
+            <p>Informasi Nutrisi</p>
+          </div>
+          <div class="row justify-content-center text-center" id="info">
+            <div class="col-3">
+              <h3>{{ nutrisiMenu[0].menu_4.calories }}</h3>
+              <p>Kalori</p>
+            </div>
+            <div class="col-3">
+              <h3>{{ nutrisiMenu[0].menu_4.carbo }}</h3>
+              <p>Karbo</p>
+            </div>
+            <div class="col-3">
+              <h3>{{ nutrisiMenu[0].menu_4.protein }}</h3>
               <p>Protein</p>
             </div>
             <div class="col-3">
@@ -59,27 +152,96 @@ import axios from "axios";
 export default {
   data() {
     return {
-      menuDietSiang: [],
+      menuDietSiang_1: [],
+      menuDietSiang_2: [],
+      menuDietSiang_3: [],
+      menuDietSiang_4: [],
+      nutrisiMenu: [
+        {
+          menu_1: {
+            carbo: "9.4",
+            calories: "65",
+            protein: "2.8",
+          },
+          menu_2: {
+            carbo: "21",
+            calories: "105",
+            protein: "3.4",
+          },
+          menu_3: {
+            carbo: "10",
+            calories: "78",
+            protein: "1.7",
+          },
+          menu_4: {
+            carbo: "12",
+            calories: "122",
+            protein: "8.1",
+          },
+        },
+      ],
     };
   },
 
   mounted() {
     let self = this;
-    const options = {
+    const menuSiang_1 = {
       method: "GET",
-      url: "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata",
+      url: "https://www.themealdb.com/api/json/v1/1/search.php?s=egg",
     };
-
+    const menuSiang_2 = {
+      method: "GET",
+      url: "https://edamam-food-and-grocery-database.p.rapidapi.com/parser",
+      params: { ingr: " cereals" },
+      headers: {
+        "X-RapidAPI-Key": "3586c6245bmsh597c24e5b8b4725p17fd36jsne5acb64e2ce1",
+        "X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com",
+      },
+    };
+    const menuSiang_3 = {
+      method: "GET",
+      url: "https://www.themealdb.com/api/json/v1/1/search.php?s=banana",
+    };
+    const menuSiang_4 = {
+      method: "GET",
+      url: "https://edamam-food-and-grocery-database.p.rapidapi.com/parser",
+      params: { ingr: "milk" },
+      headers: {
+        "X-RapidAPI-Key": "3586c6245bmsh597c24e5b8b4725p17fd36jsne5acb64e2ce1",
+        "X-RapidAPI-Host": "edamam-food-and-grocery-database.p.rapidapi.com",
+      },
+    };
     axios
-      .request(options)
+      .request(menuSiang_1)
       .then(function (response) {
-        self.menuDietSiang = response.data.meals;
-        console.log(response.data.meals);
-        // self.menuDietSiang = response.data.hints;
-        // if (self.menuMakanan.length > 10) {
-        //   self.menuMakanan.splice(0, 15);
-        //   console.log(self.menuDietSiang);
-        // }
+        self.menuDietSiang_1 = response.data.meals;
+        self.menuDietSiang_1.splice(1, 4);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    axios
+      .request(menuSiang_2)
+      .then(function (response) {
+        self.menuDietSiang_2 = response.data.hints;
+        self.menuDietSiang_2.splice(1, 21);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    axios
+      .request(menuSiang_3)
+      .then(function (response) {
+        self.menuDietSiang_3 = response.data.meals;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    axios
+      .request(menuSiang_4)
+      .then(function (response) {
+        self.menuDietSiang_4 = response.data.hints;
+        self.menuDietSiang_4.splice(1, 21);
       })
       .catch(function (error) {
         console.error(error);
