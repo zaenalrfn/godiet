94b53f9e6d91fe3cac85be2aabc0a185004e1c4e
zaenalrfn <ZAOfficial24@gmail.com>
2023-01-31 17:40:33 +0700

    manggil api menu makakan

--- src/views/MenuDietSiang.vue
+++ src/views/MenuDietSiang.vue
@@ -18,17 +18,21 @@
 
     <div class="opsiMenu">
       <div class="row justify-content-center gap-4 mb-3 pb-3">
-        <div class="cardMenu">
+        <div
+          class="cardMenu"
+          v-for="makanan in menuDietSiang"
+          v-bind:key="makanan.idMeal"
+        >
           <div class="d-flex justify-content-center">
-            <img src="" />
+            <img :src="makanan.strMealThumb" />
           </div>
           <div class="lh-sm" id="text">
-            <h4>{{}}</h4>
+            <h4>{{ makanan.strMeal }}</h4>
             <p>Informasi Nutrisi</p>
           </div>
           <div class="row justify-content-center text-center" id="info">
             <div class="col-3">
-              <h3>23</h3>
+              <h3>{{}}</h3>
               <p>Kalori</p>
             </div>
             <div class="col-3">
@@ -36,7 +40,7 @@
               <p>Karbo</p>
             </div>
             <div class="col-3">
-              <h3>1,3</h3>
+              <h3>{{}}</h3>
               <p>Protein</p>
             </div>
             <div class="col-3">
@@ -51,5 +55,35 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      menuDietSiang: [],
+    };
+  },
+
+  mounted() {
+    let self = this;
+    const options = {
+      method: "GET",
+      url: "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata",
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        self.menuDietSiang = response.data.meals;
+        console.log(response.data.meals);
+        // self.menuDietSiang = response.data.hints;
+        // if (self.menuMakanan.length > 10) {
+        //   self.menuMakanan.splice(0, 15);
+        //   console.log(self.menuDietSiang);
+        // }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  },
+};
 </script>
