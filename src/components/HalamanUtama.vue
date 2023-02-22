b4cb727d76d251e90c5c884a15cad9d6dfcd7df9
zaenalrfn <ZAOfficial24@gmail.com>
2023-02-22 10:57:16 +0700

    tes pwa bisa/tidak

--- src/components/HalamanUtama.vue
+++ src/components/HalamanUtama.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="container">
+    <v-banner v-if="deferredPrompt" color="info" dark class="text-left">
+      Get our free app. It won't take up space on your phone and also works
+      offline!
+
+      <template v-slot:actions>
+        <v-btn text @click="dismiss">Dismiss</v-btn>
+        <v-btn text @click="install">Install</v-btn>
+      </template>
+    </v-banner>
     <div id="header">
       <div class="row justify-content-between mt-4 gap-3">
         <div class="col">
@@ -110,7 +119,26 @@ export default {
     return {
       gambarProfil: null,
       getProfilEmail: null,
+      deferredPrompt: null,
     };
+  },
+  created() {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      this.deferredPrompt = e;
+    });
+    window.addEventListener("appinstalled", () => {
+      this.deferredPrompt = null;
+    });
+  },
+  methods: {
+    async dismiss() {
+      this.deferredPrompt = null;
+    },
+    async install() {
+      this.deferredPrompt.prompt();
+    },
   },
   mounted() {
     if (localStorage.getItem("url-gambar") || localStorage.getItem("picture")) {
